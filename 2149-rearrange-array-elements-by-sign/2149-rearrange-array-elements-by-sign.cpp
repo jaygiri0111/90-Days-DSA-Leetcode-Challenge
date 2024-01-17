7bf7d9a4b342e3ab9d81ca220c9dfdf4dac5a57e
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-01-17 22:49:42 +0530

    Time: 186 ms (14.42%), Space: 124.2 MB (64.03%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,55 +1,55 @@
-//Approach -01
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>pos;
-        vector<int>neg;
-        for(int i=0; i<n; i++)
-        {
-          if(nums[i]<0)
-          {
-             neg.push_back(nums[i]);
-          }
-          else
-          {
-              pos.push_back(nums[i]);
-          }
-             
-        }
-        for(int i=0; i<n/2; i++)
-        {
-          nums[2*i] = pos[i];
-          nums[2*i+1] = neg[i];   
-        }
-        return nums;
-    }
-};
-
-// //Approrach -02 (Optimal solution)
+// //Approach -01
 // class Solution {
 // public:
 //     vector<int> rearrangeArray(vector<int>& nums) {
 //         int n = nums.size();
-//         vector<int>ans(n,0);
-//         int pos = 0, neg=1;
+//         vector<int>pos;
+//         vector<int>neg;
 //         for(int i=0; i<n; i++)
 //         {
-//           if(nums[i]>0)
+//           if(nums[i]<0)
 //           {
-//             ans[pos] = nums[i];
-//               pos+=2;
+//              neg.push_back(nums[i]);
 //           }
-//          else
-//          {
-//            ans[neg] = nums[i];
-//              neg+=2;
-//          }
-            
+//           else
+//           {
+//               pos.push_back(nums[i]);
+//           }
+             
 //         }
-//         return ans;    
+//         for(int i=0; i<n/2; i++)
+//         {
+//           nums[2*i] = pos[i];
+//           nums[2*i+1] = neg[i];   
+//         }
+//         return nums;
 //     }
 // };
+
+//Approrach -02 (Optimal solution)
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(n,0);
+        int pos = 0, neg=1;
+        for(int i=0; i<n; i++)
+        {
+          if(nums[i]>0)
+          {
+            ans[pos] = nums[i];
+              pos+=2;
+          }
+         else
+         {
+           ans[neg] = nums[i];
+             neg+=2;
+         }
+            
+        }
+        return ans;    
+    }
+};
 
 
 
