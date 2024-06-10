ae0b3f92c9f47687317fc2cd9c1a68bceabca8ca
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-06-10 09:46:18 +0530

    Time: 20 ms (19.16%), Space: 21.1 MB (6.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,3 +1,21 @@
+//method 1
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+
+        return n;
+    }
+};
+
+/*
+// Method 2 using sum method
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -11,3 +29,7 @@ public:
         return sum1 - sum2;
     }
 };
+// Time complexity -> O(n)
+// space complexity -> O(1)
+
+*/
