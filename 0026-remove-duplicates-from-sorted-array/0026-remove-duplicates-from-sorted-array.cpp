f0d3309125b794542e9163f6814cca6e0f953b5b
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-06-10 12:56:46 +0530

    Time: 0 ms (100%), Space: 21.1 MB (30.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,3 +1,6 @@
+/*
+//method 01 --> In place algorithm
+//Without extra space
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -15,5 +18,33 @@ public:
             }
         }
         return index;
+    }
+};
+Time complexity = O(n)
+//space complexity = O(1)
+*/
+
+// Method 2 --> Two pointer approach
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        if (nums.empty())
+            return 0;
+
+        int n = nums.size();
+        int i = 0; //-->position of unique element
+        int j = 1;
+        while (j < n) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+
+        return i + 1;
     }
 };
