8aac5cda7606c172709666b9fd2baf61521ef4f7
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-06-11 10:19:04 +0530

    Time: 0 ms (100%), Space: 9.8 MB (71.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,64 +1,51 @@
-//Approach - 01(Brute force)
-// class Solution {
-// public:
-//     void sortColors(vector<int>& nums) {
-//         //sort(nums.begin(),nums.end());//--->O(nlogn)
-//         int count0=0,count1=0,count2=0;
-//         for(int i=0; i<nums.size(); i++)//--->O(n)
-//         {
-//             if(nums[i]==0)
-//             {
-//                 count0++;
-//             }
-//             if(nums[i]==1)
-//             {
-//                 count1++;
-//             }
-//             if(nums[i]==2)
-//             {
-//                 count2++;
-//             }
-//         }
-//         for(int i =0; i<count0; i++)
-//         {
-//             nums[i]=0;
-//         }
-//         for(int i =count0; i<count0+count1; i++)
-//         {
-//             nums[i]=1;
-//         }
-//         for(int i =count0+count1; i<nums.size(); i++)
-//         {
-//             nums[i]=2;
-//         }
-//     }
-// };
-
-//Approach - 02
-/*-----------------------------------------------------------------------*/
-/*Dutch flag algorithm with concept of three pointer two pointer starts with index 0 and another one pointing to the last and comes in decreasing manner*/
+/*
+// method - 1
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-      int n = nums.size();
-       int low =0, mid =0, high = n-1;
-       for(int i=0; i<n; i++)
-       {
-           if(nums[mid]==0)
-           {
-               swap(nums[low],nums[mid]);
-               low++;
-               mid++;
-           }
-           else if(nums[mid]==1)
-           {
-               mid++;
-           }
-           else if(nums[mid]==2)
-           {
-               swap(nums[mid],nums[high]);
-               high--;
-           }
-         } 
+        int n = nums.size();
+        int zero_count = 0;
+        int one_count = 0;
+        int two_count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                zero_count++;
+            } else if (nums[i] == 1) {
+                one_count++;
+            } else {
+                two_count++;
+            }
+        }
+        for (int i = 0; i < zero_count; i++) {
+            nums[i] = 0;
+        }
+        for (int i = zero_count; i < zero_count + one_count; i++) {
+            nums[i] = 1;
+        }
+        for (int i = zero_count + one_count; i < n; i++) {
+            nums[i] = 2;
+        }
+    }
+};
+*/
+
+// method - 02 --> Dutch Flag Algo
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
