ab45590a5bf2c18372fdc9bd9edf1789a7bac123
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-06-13 11:23:08 +0530

    Time: 27 ms (57.08%), Space: 29.9 MB (66.39%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int start = 0;
         int end = n - 1;
-        int mid = (start + end) / 2;
+        int mid = start+(end-start) / 2;
         while (start <= end) {
             if (nums[mid] == target) {
                 return mid;
@@ -15,7 +15,7 @@ public:
             } else if (nums[mid] > target) {
                 end = mid - 1;
             }
-            mid = (start + end) / 2;
+            mid = start+(end-start) / 2;
         }
         return -1;
     }
