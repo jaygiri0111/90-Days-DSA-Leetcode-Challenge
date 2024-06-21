4de27df76e787435649026e18394886671b742dd
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-06-21 12:41:29 +0530

    Time: 82 ms (34.32%), Space: 70.3 MB (30%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,69 +1,15 @@
-//Approach - 01(Brute Force) Time complexity - O(n^2)
-/*class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int maxi = nums[0];
-        for(int i = 0; i<n; i++)
-        { int sum = 0;
-           for(int j =i; j<n; j++)
-           {
-             sum += nums[j];
-             maxi = max(sum, maxi);
-           }
-        }
-        return maxi;
-    }
-};
-*/
-//Approach -02  (Optimal Solution) Time complexity - O(n)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0;
         int n = nums.size();
-        int maxi = nums[0];
-        for(int i = 0; i<n; i++)
-        {
-         sum = sum + nums[i];
-         maxi = max(sum,maxi);
-         if(sum < 0) 
-         {
-             sum = 0;
-         }
+        int sum = 0, ans = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            ans = max(ans, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
         }
-        return maxi;
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
