f3b7e1a5f74e15f04e6162280bd9858c2ef69b64
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-06-27 00:51:47 +0530

    Time: 26 ms (11%), Space: 24.7 MB (14.06%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,58 +1,66 @@
-// // Approach -01(Brute force )
-// class Solution {
-// public:
-//     int singleNonDuplicate(vector<int>& nums) {
-//         int n = nums.size();
-//         int i = 0;
-//         if (n == 1)
-//             return nums[0];
-//         for (int i = 0; i < n; i++) {
-//             if (i == 0) {
-//                 if (nums[i] != nums[i + 1]) {
-//                     return nums[i];
-//                 }
-//             } else if (i == n - 1) {
-//                 if (nums[n - 1] != nums[n - 2]) {
-//                     return nums[n - 1];
-//                 }
-//             } else {
-//                 if (nums[i] != nums[i + 1] && nums[i] != nums[i - 1]) {
-//                     return nums[i];
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
-// Approach -02 (Binary Search)
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1)
-            return nums[0];
-        if (nums[0] != nums[1])
-            return nums[0];
-       if(nums[n-1]!=nums[n-2])
-           return nums[n - 1];
-       int low = 1, high = n - 2;
-       while (low <= high) {
-           int mid = (low + high) / 2;
-           if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
-               return nums[mid];
-           } else {
-               if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
-                   (mid % 2 == 0 && nums[mid] == nums[mid+1]))//(even, odd)-->element on right
-                                                              //(odd, even)---> element on left side
-            {
-                low = mid+1;
+        int s = 0;
+        int e = n - 1;
+        int mid = s + (e - s) / 2;
+        while (s <= e) {
+            // if there is only one element
+            if (s == e) {
+                return nums[s];
             }
-               else {
-                   high = mid - 1;
-               }
-           }
-       }
-       return -1;
+            int currentIndex = nums[mid];
+            int leftIndex = -1;
+            int rightIndex = -1;
+            if(mid-1>=0)
+            {
+                leftIndex = nums[mid-1];
+            }
+            if(mid<n)
+            {
+                rightIndex = nums[mid+1];
+            }
+            // No single element
+            if (currentIndex!= leftIndex && currentIndex != rightIndex)
+            {
+                   return currentIndex;
+            }
+            //left side element 
+            if(currentIndex == leftIndex)
+            {
+                int startIndex = mid-1;
+                if(startIndex&1)
+                {
+                    //odd index
+                    //move left side
+                    e = mid-1;
+                }
+                else
+                {
+                    //even Index 
+                    //move right side
+                    s = mid+1;
+                }
+            }
+              if(currentIndex == rightIndex)
+            {
+                int startIndex = mid;
+                if(startIndex&1)
+                {
+                    //odd index
+                    //move left side
+                    e = mid-1;
+                }
+                else
+                {
+                    //even Index 
+                    //move right side
+                    s = mid+1;
+                }
+            }
+            mid = s + (e - s) / 2;
+        }
+        return -1;
     }
 };
