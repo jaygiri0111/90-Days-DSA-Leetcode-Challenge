225fee1b50b99a71bc8e9c555aae4b51a3d90eac
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-07-01 12:00:47 +0530

    Time: 0 ms (100%), Space: 7.5 MB (6.09%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,21 +2,27 @@ class Solution {
 public:
     int mySqrt(int x) {
         int s = 0;
-        int e = x;
-        long long int mid = s + (e - s) / 2;
-        int ans;
-        while (s <= e) {
-            long long int product = mid * mid;
-            if (product == x) {
+        int e =x;
+        int ans = -1;
+        while(s<=e)
+        {
+            long long int  mid = s+(e-s)/2;
+            long long int product = mid*mid;
+            if(product == x)
+            {
                 return mid;
-            } else if (product > x) {
-                e = mid - 1;
-            } else {
-                ans = mid;
-                s = mid + 1;
             }
-            mid = s + (e - s) / 2;
+            else if(product<=x)
+            {
+                ans = mid;
+                s = mid+1;
+            }
+            else
+            {
+                e = mid-1;
+            }
         }
         return ans;
+        
     }
 };
