f09ec9bc7cbc25b5471ea93c661b63ad0878d183
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-07-05 13:11:13 +0530

    Time: 0 ms (100%), Space: 11.2 MB (85.29%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,20 +1,42 @@
-class Solution {
+/*class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        
+
         // Handle cases with only one element or two elements
         if (n == 1) return 0;
         if (nums[0] > nums[1]) return 0;
         if (nums[n - 1] > nums[n - 2]) return n - 1;
-        
+
         // Handle general case
         for (int i = 1; i < n - 1; i++) {
             if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
                 return i;
             }
         }
-        
+
         return -1; // This should not be reached for a valid mountain array
+    }
+};
+*/
+// T.C = O(n)
+// S.C = O(1)
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return end; // This should not be reached for a valid mountain array
     }
 };
