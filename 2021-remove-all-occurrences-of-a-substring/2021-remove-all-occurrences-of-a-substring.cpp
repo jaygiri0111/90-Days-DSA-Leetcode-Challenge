8fbe36506a8aa7abd98ee41f8ed2d03c230eef59
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-07-19 10:04:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (14.54%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeOccurrences(string full, string pattern) {
+
+        while(full.find(pattern)!=string::npos)
+        {
+            full.erase(full.find(pattern), pattern.length());
+        }
+        return full;
+    }
+};
