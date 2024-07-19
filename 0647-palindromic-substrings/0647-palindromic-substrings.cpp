39cac533b915a1a90630d728d3917dc7dc9e94e0
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-07-19 11:46:07 +0530

    Time: 11 ms (56.13%), Space: 21.7 MB (24.34%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int expandcenter(string s, int i, int j) {
+        int count = 0;
+        while (i >= 0 && j < s.length() && s[i] == s[j]) {
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int totalcount = 0;
+        for (int center = 0; center < s.length(); center++) {
+            int i = center;
+            int j = center;
+            // odd length substring
+            int oddpalindsubstringkacount = expandcenter(s, i, j);
+            i = center;
+            j = center + 1;
+            // even length substring
+            int evenpalindkacount = expandcenter(s, i, j);
+            totalcount =
+                totalcount + oddpalindsubstringkacount + evenpalindkacount;
+        }
+        return totalcount;
+    }
+};
