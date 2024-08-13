a5c7acbd3743a2a2387ccf0083f81f06a464f565
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-08-13 23:51:37 +0530

    Time: 19 ms (48.82%), Space: 28 MB (13.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,130 +1,19 @@
-/* Time complexity = O(n^2)
-   This solution may face TLE(Time Limit Exceed) Problem 
-*/
-/*class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-    
-        int n = nums.size();
-        for(int i=0; i<n; i++)
-        {  
-            int count =0;
-            for(int j = 0; j<n; j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                    count++;
-                }
-            }
-            
-            if(count > n/2)
-            {
-            return nums[i];
-            }
-        }
-         return -1;
-    }
-};
-
-*/
-
-
-/*Approach - 02 (Better Solution)
-  Time Complexity - O(nlogn)
-*/
-/*class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-    int n = nums.size();
-     map<int, int>mp;
-     for(int i=0; i<n; i++)
-     {
-         mp[nums[i]]++;
-     } 
-    for(auto it : mp)
-    {
-        if(it.second>nums.size()/2)
-        {
-            return it.first;
-        }
-    }  
-    return -1;
-    }
-};
-*/
-
-/*Optimal Solution -> time complexity -> O(n)
--> Applying Moore's Voting Algorihtm
-*/
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    
-        int n = nums.size(), count =0, value;
-       for(int i=0; i<n; i++)
-       {
-           if(count ==0)
-           {
-               value = nums[i];
-               count++;
-           }
-           else if(nums[i]==value)
-           {
-               count++;
-           }
-           else
-           {
-               count--;
-           }
-       }
-        int count1=0;
-        for(int i=0; i<n; i++)
+        unordered_map<int, int>mpp;
+        int n = nums.size();
+        for(int i=0; i<nums.size(); i++)
         {
-          if(nums[i]==value)
-          {
-              count1++;
-          }
+            mpp[nums[i]]++;
         }
-        if(count1>n/2)
+        for(auto it : mpp)
         {
-            return value;
+            if(it.second>(n/2))
+            {
+                return it.first;
+            }
         }
         return -1;
     }
 };
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
