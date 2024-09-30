63d2cddf899fa37561e304f9f4619bd47c6fd977
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-09-30 13:11:47 +0530

    Time: 0 ms (100%), Space: 10.3 MB (31.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,51 +1,26 @@
-/*
-// method - 1
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int zero_count = 0;
-        int one_count = 0;
-        int two_count = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                zero_count++;
-            } else if (nums[i] == 1) {
-                one_count++;
-            } else {
-                two_count++;
-            }
+        int low=0, mid=0 ,high=n-1;
+       while(mid<=high)
+       {
+        if(nums[mid]==0)
+        {
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
         }
-        for (int i = 0; i < zero_count; i++) {
-            nums[i] = 0;
+        else if(nums[mid]==1)
+        {
+            mid++;
         }
-        for (int i = zero_count; i < zero_count + one_count; i++) {
-            nums[i] = 1;
+        else if(nums[mid]==2)
+        {
+          swap(nums[mid],nums[high]);
+          high--;
         }
-        for (int i = zero_count + one_count; i < n; i++) {
-            nums[i] = 2;
-        }
-    }
-};
-*/
+       }
 
-// method - 02 --> Dutch Flag Algo
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0, mid = 0, high = n - 1;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else if (nums[mid] == 2) {
-                swap(nums[mid], nums[high]);
-                high--;
-            }
-        }
     }
 };
