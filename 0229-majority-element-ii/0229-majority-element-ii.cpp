2044f77ebfe047f44bf5ad21cf4c113410eed2bf
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-10-04 10:08:03 +0530

    Time: 12 ms (30.81%), Space: 19.8 MB (22.61%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,45 +1,20 @@
-// class Solution {
-// public:
-//     vector<int> majorityElement(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> ans;
-//         for (int i = 0; i < n; i++) {
-
-//             if (ans.size() == 0 || ans[0] != nums[i]) {
-//                 int count = 0;
-//                 for (int j = 0; j < n; j++) {
-//                     if (nums[i] == nums[j]) {
-//                         count++;
-//                     }
-//                 }
-//                 if (count > (n / 3)) {
-//                     ans.push_back(nums[i]);
-//                 }
-//             }
-//             if (ans.size() == 2)
-//                 break;
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
         unordered_map<int,int>mpp;
-        vector<int> ans;
-        int mini = ceil(n/3);
-        for (int i = 0; i < n; i++) {
-          mpp[nums[i]]++;
-        }
-        for(auto it : mpp)
+        int n = nums.size();
+        vector<int>ans;
+        for(int i=0; i<n; i++)
         {
-            if(it.second>(n/3))
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second>n/3)
             {
                 ans.push_back(it.first);
             }
         }
-        return ans;
+            return ans;;      
     }
 };
