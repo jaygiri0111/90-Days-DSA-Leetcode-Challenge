05651b29ea2b3a8158883a4d35675b17bbdd7027
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-10-15 13:21:21 +0530

    Time: 11 ms (33.8%), Space: 21.1 MB (96.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,48 +1,13 @@
-//method 01 --> In place algorithm
-//Without extra space
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
-        if (nums.empty())
-            return 0;
-
-        int index = 1;
-        int n = nums.size();
-        for (int i = 1; i < n; i++) {
+        int k = 1;
+        for (int i = 1; i <nums.size(); i++) {
             if (nums[i] != nums[i-1]) {
-
-                nums[index] = nums[i];
-                index++;
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return index;
+        return k;
     }
 };
-//Time complexity = O(n)
-//space complexity = O(1)
-
-// Method 2 --> Two pointer approach
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums) {
-
-//         if (nums.empty())
-//             return 0;
-
-//         int n = nums.size();
-//         int i = 0; //-->position of unique element
-//         int j = 1;
-//         while (j < n) {
-//             if (nums[i] == nums[j]) {
-//                 j++;
-//             } else {
-//                 i++;
-//                 nums[i] = nums[j];
-//                 j++;
-//             }
-//         }
-
-//         return i + 1;
-//     }
-// };
