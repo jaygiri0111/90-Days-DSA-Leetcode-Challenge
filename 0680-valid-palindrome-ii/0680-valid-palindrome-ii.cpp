52145313291bafee6f76ba62e1dfc3b7891d9bff
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-10-15 12:42:57 +0530

    Time: 52 ms (72.35%), Space: 26.5 MB (20.79%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     bool checkpalindrome(string str, int s, int e) {
         while (s <= e) {
-            if (str[s] == str[e]) {
+            if (str[s] != str[e]) {
+                return false;
+            } else {
                 s++;
                 e--;
-            }
-            else
-            {
-                return false;
             }
         }
         return true;
     }
     bool validPalindrome(string s) {
-        int i = 0;
-        int j = s.length() - 1;
+        int n = s.size();
+        int i = 0, j = n - 1;
         while (i <= j) {
-            // if same
             if (s[i] == s[j]) {
                 i++;
                 j--;
             }
-            // if different
-            else {
-                //deleting ith index and checking for palindrome
-                bool ansOne = checkpalindrome(s, i + 1, j);
-                //deleting jth index and checking for palindrome
-                bool ansTwo = checkpalindrome(s, i, j - 1);
-                //checking which part is valid palindrome
-                bool finalAns = ansOne || ansTwo;
-                return finalAns;
+            else
+            {
+                bool ansone = checkpalindrome(s,i+1,j);
+                bool anstwo = checkpalindrome(s,i,j-1);
+                bool finalans = ansone||anstwo;
+                return finalans;
             }
+            
         }
         return true;
     }
