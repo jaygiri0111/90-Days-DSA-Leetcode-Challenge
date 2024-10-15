e984cab91a48eff1169db4d9a80996cb3f73ee75
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-10-15 12:21:35 +0530

    Time: 5 ms (46.58%), Space: 8.2 MB (23.92%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    string removeOccurrences(string full, string pattern) {
-
-       //jab tak pattern full string mae mil rahe hai, jab tak -1 ke equal nahi hai while loop chalta rahega 
-        while(full.find(pattern)!=string::npos)
+    string removeOccurrences(string s, string part) {
+        while(s.find(part)!=string::npos)
         {
-            //erase karo starting index of pattern to length of  pattern 
-            full.erase(full.find(pattern), pattern.length());
+            s.erase(s.find(part),part.length());
         }
-        return full;
+        return s;
     }
 };
