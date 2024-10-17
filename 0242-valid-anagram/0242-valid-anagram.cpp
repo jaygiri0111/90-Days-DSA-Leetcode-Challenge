904aac494e0cfa04f04182223f1a39c19f4a7996
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-10-17 12:18:54 +0530

    Time: 7 ms (76.12%), Space: 9 MB (42.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t){
+        int n1 = s.size();
+        int n2 = t.size();
+        if(n1!=n2)
+        {
             return false;
         }
+        int hashtables[256]={0};
+        for (int i = 0; i < n1; i++) {
+            hashtables[s[i]]++;
+        }
+        for (int i = 0; i < n2; i++) {
+            hashtables[t[i]]--;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (hashtables[i] != 0) {
+                return false;
+            }
+        }
+
         return true;
-        
     }
 };
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//        sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         if(s!=t){
+//             return false;
+//         }
+//         return true;
+
+//     }
+// };
