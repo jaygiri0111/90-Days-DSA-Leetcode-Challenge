91fe99ac047b7f8b6ed1cbe61e2b1886461940ad
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-11-03 22:37:32 +0530

    Time: 0 ms (100%), Space: 12.8 MB (92.26%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int index = 0, count = 1;
+        char prev = chars[0];
+        for (int i = 1; i < chars.size(); i++) {
+            if (chars[i] == prev) {
+                count++;
+            } else {
+                chars[index++] = prev;
+                int start = index;
+                if (count > 1) {
+
+                    while (count) {
+                        chars[index++] = (count % 10) + '0';
+                        count /= 10;
+                    }
+                    reverse(chars.begin() + start, chars.begin() + index);
+                }
+                prev = chars[i];
+                count = 1;
+            }
+        }
+        chars[index++] = prev;
+        int start = index;
+        if (count > 1) {
+            while (count) {
+                chars[index++] = (count % 10) + '0';
+                count /= 10;
+            }
+            reverse(chars.begin() + start, chars.begin() + index);
+        }
+        return index;
+    }
+};
