e3f32ead328c0bbf8c6faf35b4e135aeb87a9904
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-11-06 00:28:10 +0530

    Time: 0 ms (100%), Space: 14.4 MB (20.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,42 +1,22 @@
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         vector<int> ans;
-//         int n = nums.size();
-//         sort(nums.begin(), nums.end());
-//         int low = 0, high = n - 1, sum = 0;
-//         while (low < high) {
-//             sum = nums[low] + nums[high];
-//             if (sum == target) {
-//                 ans.push_back(low);
-//                 ans.push_back(high);
-//                 return ans;
-
-//             } else if (sum > target) {
-//                 high--;
-//             } else if (sum < target) {
-//                 low++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans;
-        unordered_map<int, int> mpp;
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (mpp.find(complement) != mpp.end()) {
-                ans.push_back(mpp[complement]);
+        unordered_map<int, int> map;
+        vector<int>ans;
+        for (int i = 0; i < nums.size(); i++) {
+            int required_num = target - nums[i];
+            if(map.find(required_num)!=map.end())
+            {
+                ans.push_back(map[required_num]);
                 ans.push_back(i);
                 return ans;
             }
-            mpp[nums[i]] = i; // to store the index of current element
+            else
+            {
+                map[nums[i]]=i;
+            }
         }
         return ans;
+
     }
 };
