d98098baeb368e5ea676b277e0b0752bea2a0e36
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-11-07 22:18:53 +0530

    Time: 17 ms (9.34%), Space: 15.2 MB (6.58%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,42 +6,22 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-//         unordered_map<ListNode*, bool>mpp;
-//         ListNode* temp = head;
-
-//         while(temp!=NULL){
-//             if(mpp[temp]==true)
-//             {
-//                 return true;
-//             }
-//             else
-//             {
-//                 mpp[temp] = true;
-//             }
-//             temp = temp->next;
-//         }
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        while(fast!=NULL)
+        unordered_map<ListNode*, bool>mpp;
+        ListNode* temp = head;
+        while(temp!=NULL)
         {
-            fast = fast->next;
-            if(fast!=NULL)
+            if(mpp[temp]==true)
             {
-                fast = fast->next;
-                slow = slow->next;
+                return true;
             }
-            if(slow == fast) return true;
+            else
+            {
+                mpp[temp]=true;
+            }
+            temp = temp->next;
         }
         return false;
     }
