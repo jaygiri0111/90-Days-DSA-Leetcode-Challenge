72340aaaf8c0acfccd4bbf7a6548eabcee53f7bb
Jay prakash giri <121309617+jaygiri0111@users.noreply.github.com>
2024-11-09 22:35:13 +0530

    Time: 6 ms (72.22%), Space: 10.4 MB (74.15%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,24 +6,63 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Approach - 01(Using HashMap)
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//      unordered_map<ListNode*, bool>mpp;
+//      ListNode* temp = head;
+//      while(temp!=NULL)
+//      {
+//         if(mpp[temp]==true)
+//         {
+//           return temp;
+//         }
+//         else
+//         {
+//             mpp[temp]= true;
+//         }
+//         temp=temp->next;
+//      }
+//     return nullptr;
+//     }
+
+// };
+
+// Apporach-02(Using slow and fast pointer)
+ListNode* hascycle(ListNode* head) {
+    ListNode*slow = head;
+    ListNode*fast = head;
+    while(fast!=NULL)
+    {
+        fast=fast->next;
+        if(fast!=NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if(slow==fast)
+        {
+            return fast;
+        }
+    }
+    return nullptr;
+}
+
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-     unordered_map<ListNode*, bool>mpp;
-     ListNode* temp = head;
-     while(temp!=NULL)
-     {
-        if(mpp[temp]==true)
+    ListNode* detectCycle(ListNode* head) {
+        ListNode*loopdetected = hascycle(head);
+        if(loopdetected==NULL)
         {
-          return temp;
+            return nullptr;
         }
-        else
+        ListNode*slow = head;
+        while(slow!=loopdetected)
         {
-            mpp[temp]= true;
+            slow=slow->next;
+            loopdetected = loopdetected->next;
         }
-        temp=temp->next;
-     }
-    return nullptr;
+        return slow;
     }
-
 };
